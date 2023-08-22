6c3209da3b5387d24637b8ffc1ae61e6e2202892
Emily Embury <emilyembury@embury09da.nmsu.edu>
2023-08-22 10:19:13 -0600

    add file

---
+++ intro_class.R
@@ -0,0 +1,39 @@
+#introductory class
+ 
+#R as a calculator
+
+5 + 7
+
+10^2
+
+80/4
+
+(10-6) * 3/2
+
+#mathematical functions
+
+sqrt(16)
+Sqrt(16) #error, case sensitive
+
+exp(5) #exponential growth "e" (Nt = No * e^rt)
+10*exp(0.15*2)
+
+
+log(100) #log base e (natural log by default)
+
+log(100, base = 10)
+
+?log
+
+#creating objects
+
+x <- sqrt(16)
+x
+
+
+#reading data
+
+length <- c(27, 30, 35, 31, 40)
+length
+
+length[1:2] #view subset of data
